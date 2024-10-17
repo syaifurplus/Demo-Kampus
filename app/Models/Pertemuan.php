0c94d3ee6d20b1051e1ae361b9eedad3e06dcf13
syaifurplus <syaifurplus@gmail.com>
2024-10-17 11:04:23 +0700

    model and migration pertemuan

---
+++ app/Models/Pertemuan.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pertemuan extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql2';
+    
+    protected $table = 'pertemuan';
+}
